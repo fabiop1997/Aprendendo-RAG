99579d001b2560b32d824974637caaef099375e5
Fábio Paulo <fabiopauloect@gmail.com>
2025-08-24 10:16:42 -0300

    Alteação código e adição arquivo README

--- RAG.py
+++ RAG.py
@@ -1,8 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-#from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-
 book_iaf = PyPDFLoader("dados-texto.pdf")
 
 # Lendo o arquivo que chamamos de Book Iaf
